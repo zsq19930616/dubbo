2bb3b4b783981a899b5389245d5e10f405ed1f3f
zhangshiqiang <zhangshiqiang@gbw-china.com>
2020-08-03 09:42:01 +0800

    dubbo 14 day remoting-api buffer

---
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/buffer/Test.java
@@ -0,0 +1,11 @@
+package com.alibaba.dubbo.remoting.buffer;
+
+/**
+ * @author zhangshiqiang on 2020/8/3.
+ */
+public class Test {
+    public static void main(String[] args) {
+        ChannelBuffer channelBuffer = ChannelBuffers.dynamicBuffer();
+
+    }
+}
---
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/telnet/Test.java
@@ -0,0 +1,12 @@
+package com.alibaba.dubbo.remoting.telnet;
+
+import com.alibaba.dubbo.remoting.telnet.codec.TelnetCodec;
+
+/**
+ * @author zhangshiqiang on 2020/8/3.
+ */
+public class Test {
+    public static void main(String[] args) {
+        TelnetCodec telnetCodec = new TelnetCodec();
+    }
+}
